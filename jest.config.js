5280bc0eec621b04a3cd3da66c5fb8f0045e01ac
Simon Revill <simon.revill@vertumotors.com>
2024-11-25 12:48:53 +0000

    Adds jest-environment-jsdom package to enable testing a web environment

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
+/** @type {import('jest').Config} */
 const config = {
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  testEnvironment: "jsdom",
 };
 
 export default config;
