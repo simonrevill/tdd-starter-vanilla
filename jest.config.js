89cfe836ebc01043aa15732e8b84902172244d68
Simon Revill <simon.revill@vertumotors.com>
2024-11-25 15:46:20 +0000

    Adds ignore paths to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('jest').Config} */
 const config = {
   collectCoverage: true,
+  coveragePathIgnorePatterns: ["node_modules", "tests/test-utils"],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   testEnvironment: "jsdom",
